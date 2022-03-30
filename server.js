9074d7ee510b9fcb6fd8ef9bb91ce01d6ffa906d
hsuzach <hsuzach@gmail.com>
2022-03-29 18:14:54 -0700

    create category routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 // import sequelize connection
+const sequelize = require('./config/connection.js')
 
 const app = express()
 
@@ -9,4 +10,5 @@ app.use(express.urlencoded({ extended: true }))
 app.use(require('./routes'))
 
 // sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false })
 app.listen(process.env.PORT || 3000)
